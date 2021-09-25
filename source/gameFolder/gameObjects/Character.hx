244d4b5cb8f20633e78baf63d39c462490b9f4df
Gede Hari <harikresnagede@gmail.com>
2021-09-25 17:27:48 +0800

    Initial commit for modding

---
+++ source/gameFolder/gameObjects/Character.hx
@@ -0,0 +1,280 @@
+package gameFolder.gameObjects;
+
+/**
+	The character class initialises any and all characters that exist within gameplay. For now, the character class will
+	stay the same as it was in the original source of the game. I'll most likely make some changes afterwards though!
+**/
+import flixel.FlxG;
+import flixel.addons.util.FlxSimplex;
+import flixel.animation.FlxBaseAnimation;
+import flixel.graphics.frames.FlxAtlasFrames;
+import gameFolder.meta.*;
+import gameFolder.meta.data.*;
+import gameFolder.meta.data.dependency.FNFSprite;
+import gameFolder.meta.state.PlayState;
+import openfl.utils.Assets as OpenFlAssets;
+
+using StringTools;
+
+class Character extends FNFSprite
+{
+	// By default, this option set to FALSE will make it so that the character only dances twice per major beat hit
+	// If set to on, they will dance every beat, such as Skid and Pump
+	public var quickDancer:Bool = false;
+
+	public var debugMode:Bool = false;
+
+	public var isPlayer:Bool = false;
+	public var curCharacter:String = 'bf';
+
+	public var holdTimer:Float = 0;
+
+	public function new(x:Float, y:Float, ?character:String = "bf", ?isPlayer:Bool = false)
+	{
+		super(x, y);
+
+		curCharacter = character;
+		this.isPlayer = isPlayer;
+
+		var tex:FlxAtlasFrames;
+		antialiasing = (!Init.trueSettings.get('Disable Antialiasing'));
+
+		switch (curCharacter)
+		{
+			case 'gf':
+				// GIRLFRIEND CODE
+				tex = Paths.getSparrowAtlas('characters/GF_assets');
+				frames = tex;
+				animation.addByPrefix('cheer', 'GF Cheer', 24, false);
+				animation.addByPrefix('singLEFT', 'GF left note', 24, false);
+				animation.addByPrefix('singRIGHT', 'GF Right Note', 24, false);
+				animation.addByPrefix('singUP', 'GF Up Note', 24, false);
+				animation.addByPrefix('singDOWN', 'GF Down Note', 24, false);
+				animation.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "", 24, false);
+				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+				animation.addByIndices('hairBlow', "GF Dancing Beat Hair blowing", [0, 1, 2, 3], "", 24);
+				animation.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
+				animation.addByPrefix('scared', 'GF FEAR', 24);
+
+				playAnim('danceRight');
+
+			case 'dad':
+				// DAD ANIMATION LOADING CODE
+				tex = Paths.getSparrowAtlas('characters/DADDY_DEAREST');
+				frames = tex;
+				animation.addByPrefix('idle', 'Dad idle dance', 24, false);
+				animation.addByPrefix('singUP', 'Dad Sing Note UP', 24);
+				animation.addByPrefix('singRIGHT', 'Dad Sing Note RIGHT', 24);
+				animation.addByPrefix('singDOWN', 'Dad Sing Note DOWN', 24);
+				animation.addByPrefix('singLEFT', 'Dad Sing Note LEFT', 24);
+
+				playAnim('idle');
+
+			case 'bf':
+				frames = Paths.getSparrowAtlas('characters/BOYFRIEND');
+
+				animation.addByPrefix('idle', 'BF idle dance', 24, false);
+				animation.addByPrefix('singUP', 'BF NOTE UP0', 24, false);
+				animation.addByPrefix('singLEFT', 'BF NOTE LEFT0', 24, false);
+				animation.addByPrefix('singRIGHT', 'BF NOTE RIGHT0', 24, false);
+				animation.addByPrefix('singDOWN', 'BF NOTE DOWN0', 24, false);
+				animation.addByPrefix('singUPmiss', 'BF NOTE UP MISS', 24, false);
+				animation.addByPrefix('singLEFTmiss', 'BF NOTE LEFT MISS', 24, false);
+				animation.addByPrefix('singRIGHTmiss', 'BF NOTE RIGHT MISS', 24, false);
+				animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
+				animation.addByPrefix('hey', 'BF HEY', 24, false);
+				animation.addByPrefix('scared', 'BF idle shaking', 24);
+
+				playAnim('idle');
+
+				flipX = true;
+
+			case 'bf-dead':
+				frames = Paths.getSparrowAtlas('characters/BF_DEATH');
+
+				animation.addByPrefix('firstDeath', "BF dies", 24, false);
+				animation.addByPrefix('deathLoop', "BF Dead Loop", 24, true);
+				animation.addByPrefix('deathConfirm', "BF Dead confirm", 24, false);
+
+				playAnim('firstDeath');
+
+				flipX = true;
+
+			default:
+				// set up animations if they aren't already
+
+				// fyi if you're reading this this isn't meant to be well made, it's kind of an afterthought I wanted to mess with and
+				// I'm probably not gonna clean it up and make it an actual feature of the engine I just wanted to play other people's mods but not add their files to
+				// the engine because that'd be stealing assets
+				var fileNew = curCharacter + 'Anims';
+				if (OpenFlAssets.exists(Paths.offsetTxt(fileNew)))
+				{
+					var characterAnims:Array<String> = CoolUtil.coolTextFile(Paths.offsetTxt(fileNew));
+					var characterName:String = characterAnims[0].trim();
+					frames = Paths.getSparrowAtlas('characters/$characterName');
+					for (i in 1...characterAnims.length)
+					{
+						var getterArray:Array<Array<String>> = CoolUtil.getAnimsFromTxt(Paths.offsetTxt(fileNew));
+						animation.addByPrefix(getterArray[i][0], getterArray[i][1].trim(), 24, false);
+					}
+				}
+				else
+				{
+					// DAD ANIMATION LOADING CODE
+					tex = Paths.getSparrowAtlas('characters/DADDY_DEAREST');
+					frames = tex;
+					animation.addByPrefix('idle', 'Dad idle dance', 30, false);
+					animation.addByPrefix('singUP', 'Dad Sing Note UP', 24);
+					animation.addByPrefix('singRIGHT', 'Dad Sing Note RIGHT', 24);
+					animation.addByPrefix('singDOWN', 'Dad Sing Note DOWN', 24);
+					animation.addByPrefix('singLEFT', 'Dad Sing Note LEFT', 24);
+
+					playAnim('idle');
+				}
+		}
+
+		// set up offsets cus why not
+		if (OpenFlAssets.exists(Paths.offsetTxt(curCharacter + 'Offsets')))
+		{
+			var characterOffsets:Array<String> = CoolUtil.coolTextFile(Paths.offsetTxt(curCharacter + 'Offsets'));
+			for (i in 0...characterOffsets.length)
+			{
+				var getterArray:Array<Array<String>> = CoolUtil.getOffsetsFromTxt(Paths.offsetTxt(curCharacter + 'Offsets'));
+				addOffset(getterArray[i][0], Std.parseInt(getterArray[i][1]), Std.parseInt(getterArray[i][2]));
+			}
+		}
+
+		dance();
+
+		if (isPlayer) // fuck you ninjamuffin lmao
+		{
+			flipX = !flipX;
+
+			// Doesn't flip for BF, since his are already in the right place???
+			if (!curCharacter.startsWith('bf'))
+				flipLeftRight();
+			//
+		}
+		else if (curCharacter.startsWith('bf'))
+			flipLeftRight();
+	}
+
+	function flipLeftRight():Void
+	{
+		// get the old right sprite
+		var oldRight = animation.getByName('singRIGHT').frames;
+
+		// set the right to the left
+		animation.getByName('singRIGHT').frames = animation.getByName('singLEFT').frames;
+
+		// set the left to the old right
+		animation.getByName('singLEFT').frames = oldRight;
+
+		// insert ninjamuffin screaming I think idk I'm lazy as hell
+
+		if (animation.getByName('singRIGHTmiss') != null)
+		{
+			var oldMiss = animation.getByName('singRIGHTmiss').frames;
+			animation.getByName('singRIGHTmiss').frames = animation.getByName('singLEFTmiss').frames;
+			animation.getByName('singLEFTmiss').frames = oldMiss;
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (!curCharacter.startsWith('bf'))
+		{
+			if (animation.curAnim.name.startsWith('sing'))
+			{
+				holdTimer += elapsed;
+			}
+
+			var dadVar:Float = 4;
+			if (holdTimer >= Conductor.stepCrochet * dadVar * 0.001)
+			{
+				dance();
+				holdTimer = 0;
+			}
+		}
+
+		var curCharSimplified:String = simplifyCharacter();
+		switch (curCharSimplified)
+		{
+			case 'gf':
+				if (animation.curAnim.name == 'hairFall' && animation.curAnim.finished)
+					playAnim('danceRight');
+				if ((animation.curAnim.name.startsWith('sad')) && (animation.curAnim.finished))
+					playAnim('danceLeft');
+		}
+
+		// Post idle animation (think Week 4 and how the player and mom's hair continues to sway after their idle animations are done!)
+		if (animation.curAnim.finished && animation.curAnim.name == 'idle')
+		{
+			// We look for an animation called 'idlePost' to switch to
+			if (animation.getByName('idlePost') != null)
+				// (( WE DON'T USE 'PLAYANIM' BECAUSE WE WANT TO FEED OFF OF THE IDLE OFFSETS! ))
+				animation.play('idlePost', true, false, 0);
+		}
+
+		super.update(elapsed);
+	}
+
+	private var danced:Bool = false;
+
+	/**
+	 * FOR GF DANCING SHIT
+	 */
+	public function dance(?forced:Bool = false)
+	{
+		if (!debugMode)
+		{
+			var curCharSimplified:String = simplifyCharacter();
+			switch (curCharSimplified)
+			{
+				case 'gf':
+					if ((!animation.curAnim.name.startsWith('hair')) && (!animation.curAnim.name.startsWith('sad')))
+					{
+						danced = !danced;
+
+						if (danced)
+							playAnim('danceRight', forced);
+						else
+							playAnim('danceLeft', forced);
+					}
+				default:
+					// Left/right dancing, think Skid & Pump
+					if (animation.getByName('danceLeft') != null && animation.getByName('danceRight') != null)
+						playAnim((animation.curAnim.name == 'danceRight') ? 'danceLeft' : 'danceRight', forced);
+					// Play normal idle animations for all other characters
+					else
+						playAnim('idle', forced);
+			}
+		}
+	}
+
+	override public function playAnim(AnimName:String, Force:Bool = false, Reversed:Bool = false, Frame:Int = 0):Void
+	{
+		super.playAnim(AnimName, Force, Reversed, Frame);
+
+		if (curCharacter == 'gf')
+		{
+			if (AnimName == 'singLEFT')
+				danced = true;
+			else if (AnimName == 'singRIGHT')
+				danced = false;
+
+			if (AnimName == 'singUP' || AnimName == 'singDOWN')
+				danced = !danced;
+		}
+	}
+
+	public function simplifyCharacter():String
+	{
+		var base = curCharacter;
+
+		if (base.startsWith('gf'))
+			base = 'gf';
+		return base;
+	}
+}
---
+++ source/gameFolder/gameObjects/Stage.hx
@@ -0,0 +1,284 @@
+package gameFolder.gameObjects;
+
+import flixel.FlxBasic;
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.addons.effects.chainable.FlxWaveEffect;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxPoint;
+import flixel.system.FlxSound;
+import flixel.text.FlxText;
+import flixel.tweens.FlxTween;
+import gameFolder.gameObjects.background.*;
+import gameFolder.meta.CoolUtil;
+import gameFolder.meta.data.Conductor;
+import gameFolder.meta.data.dependency.FNFSprite;
+import gameFolder.meta.state.PlayState;
+
+using StringTools;
+
+/**
+	This is the stage class. It sets up everything you need for stages in a more organised and clean manner than the
+	base game. It's not too bad, just very crowded. I'll be adding stages as a separate
+	thing to the weeks, making them not hardcoded to the songs.
+**/
+class Stage extends FlxTypedGroup<FlxBasic>
+{
+	public var curStage:String;
+
+	var daPixelZoom = PlayState.daPixelZoom;
+
+	public function new(curStage)
+	{
+		super();
+
+		this.curStage = curStage;
+
+		/// get hardcoded stage type if chart is fnf style
+		if (PlayState.determinedChartType == "FNF")
+		{
+			// this is because I want to avoid editing the fnf chart type
+			// custom stage stuffs will come with forever charts
+			switch (CoolUtil.spaceToDash(PlayState.SONG.song.toLowerCase()))
+			{
+				default:
+					curStage = 'stage';
+			}
+
+			PlayState.curStage = curStage;
+		}
+
+		//
+		switch (curStage)
+		{
+			default:
+				PlayState.defaultCamZoom = 0.9;
+				curStage = 'stage';
+				var bg:FNFSprite = new FNFSprite(-600, -200).loadGraphic(Paths.image('backgrounds/' + curStage + '/stageback'));
+				bg.antialiasing = (!Init.trueSettings.get('Disable Antialiasing'));
+				bg.scrollFactor.set(0.9, 0.9);
+				bg.active = false;
+
+				// add to the final array
+				add(bg);
+
+				var stageFront:FNFSprite = new FNFSprite(-650, 600).loadGraphic(Paths.image('backgrounds/' + curStage + '/stagefront'));
+				stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
+				stageFront.updateHitbox();
+				stageFront.antialiasing = (!Init.trueSettings.get('Disable Antialiasing'));
+				stageFront.scrollFactor.set(0.9, 0.9);
+				stageFront.active = false;
+
+				// add to the final array
+				add(stageFront);
+
+				var stageCurtains:FNFSprite = new FNFSprite(-500, -300).loadGraphic(Paths.image('backgrounds/' + curStage + '/stagecurtains'));
+				stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
+				stageCurtains.updateHitbox();
+				stageCurtains.antialiasing = (!Init.trueSettings.get('Disable Antialiasing'));
+				stageCurtains.scrollFactor.set(1.3, 1.3);
+				stageCurtains.active = false;
+
+				// add to the final array
+				add(stageCurtains);
+		}
+	}
+
+	// return the girlfriend's type
+	public function returnGFtype(curStage)
+	{
+		var gfVersion:String = 'gf';
+
+		/* switch (curStage)
+		{
+			case 'highway':
+				gfVersion = 'gf-car';
+			case 'mall' | 'mallEvil':
+				gfVersion = 'gf-christmas';
+			case 'school':
+				gfVersion = 'gf-pixel';
+			case 'schoolEvil':
+				gfVersion = 'gf-pixel';
+		} */
+
+		return gfVersion;
+	}
+
+	// get the dad's position
+	public function dadPosition(curStage, dad:Character, gf:Character, camPos:FlxPoint, songPlayer2):Void
+	{
+		switch (songPlayer2)
+		{
+			case 'gf':
+				dad.setPosition(gf.x, gf.y);
+				gf.visible = false;
+			/*
+				if (isStoryMode)
+				{
+					camPos.x += 600;
+					tweenCamIn();
+			}*/
+
+			case "spooky":
+				dad.y += 200;
+			case "monster":
+				dad.y += 100;
+			case 'monster-christmas':
+				dad.y += 130;
+			case 'dad':
+				camPos.x += 400;
+			case 'pico':
+				camPos.x += 600;
+				dad.y += 300;
+			case 'parents-christmas':
+				dad.x -= 500;
+			case 'senpai':
+				dad.x += 150;
+				dad.y += 360;
+				camPos.set(dad.getGraphicMidpoint().x + 300, dad.getGraphicMidpoint().y);
+			case 'senpai-angry':
+				dad.x += 150;
+				dad.y += 360;
+				camPos.set(dad.getGraphicMidpoint().x + 300, dad.getGraphicMidpoint().y);
+			case 'spirit':
+				dad.x -= 150;
+				dad.y += 100;
+				camPos.set(dad.getGraphicMidpoint().x + 300, dad.getGraphicMidpoint().y);
+			case 'tankman':
+				dad.x += 50;
+				dad.y += 200;
+		}
+	}
+
+	public function repositionPlayers(curStage, boyfriend:Character, dad:Character, gf:Character):Void
+	{
+		// REPOSITIONING PER STAGE
+		switch (curStage)
+		{
+			case 'highway':
+				boyfriend.y -= 220;
+				boyfriend.x += 260;
+
+			// resetFastCar();
+			// add(fastCar);
+
+			case 'mall':
+				boyfriend.x += 200;
+
+			case 'mallEvil':
+				boyfriend.x += 320;
+				dad.y -= 80;
+			case 'school':
+				boyfriend.x += 200;
+				boyfriend.y += 220;
+				gf.x += 180;
+				gf.y += 300;
+			case 'schoolEvil':
+				// trailArea.scrollFactor.set();
+
+				// var evilTrail = new FlxTrail(dad, null, 4, 24, 0.3, 0.069);
+				// evilTrail.changeValuesEnabled(false, false, false, false);
+				// evilTrail.changeGraphic()
+				// add(evilTrail);
+				// evilTrail.scrollFactor.set(1.1, 1.1);
+
+				boyfriend.x += 200;
+				boyfriend.y += 220;
+				gf.x += 180;
+				gf.y += 300;
+		}
+	}
+
+	var curLight:Int = 0;
+	var trainMoving:Bool = false;
+	var trainFrameTiming:Float = 0;
+
+	var trainCars:Int = 8;
+	var trainFinishing:Bool = false;
+	var trainCooldown:Int = 0;
+	var startedMoving:Bool = false;
+
+	public function stageUpdate(curBeat:Int, boyfriend:Boyfriend, gf:Character, dadOpponent:Character)
+	{
+		// trace('update backgrounds');
+		/* switch (PlayState.curStage)
+		{
+			case 'highway':
+				// trace('highway update');
+				grpLimoDancers.forEach(function(dancer:BackgroundDancer)
+				{
+					dancer.dance();
+				});
+			case 'mall':
+				upperBoppers.animation.play('bop', true);
+				bottomBoppers.animation.play('bop', true);
+				santa.animation.play('idle', true);
+
+			case 'school':
+				bgGirls.dance();
+
+			case 'philly':
+				if (!trainMoving)
+					trainCooldown += 1;
+
+				if (curBeat % 4 == 0)
+				{
+					var lastLight:FlxSprite = phillyCityLights.members[0];
+
+					phillyCityLights.forEach(function(light:FNFSprite)
+					{
+						// Take note of the previous light
+						if (light.visible == true)
+							lastLight = light;
+
+						light.visible = false;
+					});
+
+					// To prevent duplicate lights, iterate until you get a matching light
+					while (lastLight == phillyCityLights.members[curLight])
+					{
+						curLight = FlxG.random.int(0, phillyCityLights.length - 1);
+					}
+
+					phillyCityLights.members[curLight].visible = true;
+					phillyCityLights.members[curLight].alpha = 1;
+
+					FlxTween.tween(phillyCityLights.members[curLight], {alpha: 0}, Conductor.stepCrochet * .016);
+				}
+
+				if (curBeat % 8 == 4 && FlxG.random.bool(30) && !trainMoving && trainCooldown > 8)
+				{
+					trainCooldown = FlxG.random.int(-4, 0);
+					trainStart();
+				}
+		} */
+	}
+
+	public function stageUpdateConstant(elapsed:Float, boyfriend:Boyfriend, gf:Character, dadOpponent:Character)
+	{
+		/* switch (PlayState.curStage)
+		{
+			case 'philly':
+				if (trainMoving)
+				{
+					trainFrameTiming += elapsed;
+
+					if (trainFrameTiming >= 1 / 24)
+					{
+						updateTrainPos(gf);
+						trainFrameTiming = 0;
+					}
+				}
+		} */
+	}
+
+	override function add(Object:FlxBasic):FlxBasic
+	{
+		Main.loadedAssets.insert(Main.loadedAssets.length, Object);
+		return super.add(Object);
+	}
+}
